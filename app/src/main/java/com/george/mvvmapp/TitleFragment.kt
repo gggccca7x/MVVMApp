841f430d374ea10676a5c39daa9b6931715d0def
George <george@pulseguard.org>
2020-05-07 13:21:11 +0100

    added menu to title fragment

--- app/src/main/java/com/george/mvvmapp/TitleFragment.kt
+++ app/src/main/java/com/george/mvvmapp/TitleFragment.kt
@@ -1,28 +1,32 @@
 package com.george.mvvmapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.fragment_title.view.*
 
 class TitleFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         val binding = inflater.inflate(R.layout.fragment_title, container, false)
+        binding.button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_bookingFragment))
 
-        binding.button.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_bookingFragment)
-        )
+        setHasOptionsMenu(true)
 
         return binding
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 }
