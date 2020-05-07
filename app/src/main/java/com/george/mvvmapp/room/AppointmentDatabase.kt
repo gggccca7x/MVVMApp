546bb6ce423956e1d22954b72cfe35285c541ff3
George Cox <georgecox67@hotmail.co.uk>
2020-05-07 19:20:28 +0100

    created room database with singleton pattern

---
+++ app/src/main/java/com/george/mvvmapp/room/AppointmentDatabase.kt
@@ -0,0 +1,36 @@
+package com.george.mvvmapp.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Appointment::class], version = 1, exportSchema = false)
+abstract class AppointmentDatabase: RoomDatabase() {
+
+    abstract val appointmentDatabaseDao: AppointmentDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppointmentDatabase? = null
+
+        fun getInstance(context: Context) : AppointmentDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppointmentDatabase::class.java,
+                        "appointment_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+
+                return instance
+            }
+        }
+    }
+}
