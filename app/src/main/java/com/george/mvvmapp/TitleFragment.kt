1cda2c328b52529d249b0dc9b2313bb8210703a4
George <george@pulseguard.org>
2020-05-07 12:53:03 +0100

    added new fragment

---
+++ app/src/main/java/com/george/mvvmapp/TitleFragment.kt
@@ -0,0 +1,19 @@
+package com.george.mvvmapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+
+}
