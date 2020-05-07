3a3f9331d45aa59992ce62bd668b42c6512fbb1c
George <george@pulseguard.org>
2020-05-07 16:55:08 +0100

    using live data and an observer to not change the date on screen rotation

--- app/src/main/java/com/george/mvvmapp/screens/booking_fragment/BookingFragment.kt
+++ app/src/main/java/com/george/mvvmapp/screens/booking_fragment/BookingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.george.mvvmapp.R
 import com.george.mvvmapp.databinding.FragmentBookingBinding
@@ -19,6 +20,14 @@ class BookingFragment : Fragment() {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_booking, container, false)
         viewModel = ViewModelProviders.of(this).get(BookingViewModel::class.java)
+
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            viewModel.onDateChanged(year, month, dayOfMonth)
+        }
+
+        viewModel.longTime.observe(viewLifecycleOwner, Observer {
+            binding.calendarView.setDate(it, false, false)
+        })
 
 
         return binding.root
--- app/src/main/java/com/george/mvvmapp/screens/booking_fragment/BookingViewModel.kt
+++ app/src/main/java/com/george/mvvmapp/screens/booking_fragment/BookingViewModel.kt
@@ -1,15 +1,26 @@
 package com.george.mvvmapp.screens.booking_fragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class BookingViewModel : ViewModel() {
 
-    init {
+    var longTime = MutableLiveData<Long>()
 
+    init {
+        longTime.value = System.currentTimeMillis()
     }
 
     override fun onCleared() {
         super.onCleared()
 
+
+    }
+
+    fun onDateChanged(year: Int, month: Int, dayOfMonth: Int) {
+        val instance = Calendar.getInstance()
+        instance.set(year, month, dayOfMonth)
+        longTime.value = instance.timeInMillis
     }
 }
